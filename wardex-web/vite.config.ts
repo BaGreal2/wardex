f05116159647fe1cefe90e5e5cce276eacdd9c98
BaGreal2 <artemonik17@gmail.com>
2025-11-30 23:47:35 +0100

    feat: allow host in wardex-web

--- wardex-web/vite.config.ts
+++ wardex-web/vite.config.ts
@@ -3,5 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: true,
+    allowedHosts: ['wardex-vm.switzerlandnorth.cloudapp.azure.com']
+  }
 });
